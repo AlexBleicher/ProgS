3b3993f1f1dc2e628578fd10b697dac75ec818b5
AlexBleicher <alexander.bleicher01@gmail.com>
2024-10-17 11:05:36 +0200

    Implemented newton_sqrt in Haskell

--- FirstTask/Haskell/newton_sqrt/newton_sqrt.hs
+++ FirstTask/Haskell/newton_sqrt/newton_sqrt.hs
@@ -1,2 +1,9 @@
-main =  do
-        number <- getLine
+iteration x y = if (x*x-y) < 0.0001 then x else iteration (0.5 * (x + y/x)) y
+
+main = do
+    putStrLn "What number do you want to calculate the square root from?"
+    input <- getLine
+    let number = read input :: Double
+    let number1 = iteration number number
+    putStrLn ("Your square root is (approximatly): ")
+    print number1
